22cea2bd9d8ca97721732ea9ab5e63ae8fe5d000
Mitali Padiya <mdpadiya10@gmail.com>
2023-10-30 03:39:33 +0530

    Login UI

--- app/src/main/java/com/bidridego/LoginActivity.java
+++ app/src/main/java/com/bidridego/LoginActivity.java
@@ -14,8 +14,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        loginButton =findViewById(R.id.loginButton);
+        setContentView(R.layout.new_login);
         emailEditText = findViewById(R.id.emailEditText);
     }
 }
