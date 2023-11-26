bd00078c0695d036c79f75ee41f2ba123fdc3dd3
Trushit Patel <pateltrushit1710@gmail.com>
2023-11-26 15:38:43 -0500

    Trip Creation Completed

--- app/src/main/java/com/bidridego/driver/DriverTripsListFragment.java
+++ app/src/main/java/com/bidridego/driver/DriverTripsListFragment.java
@@ -4,12 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.bidridego.R;
 import com.bidridego.models.Trip;
 import com.bidridego.viewadapter.ArrayTripAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -18,26 +25,40 @@ public class DriverTripsListFragment  extends Fragment {
     private RecyclerView recyclerView;
     private ArrayTripAdapter adapter;
     public ArrayList<Trip> tripArrayList;
-    int row_index = 1 ;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReferenceToTrips;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.recyclerview_list, container, false);
 
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReferenceToTrips = firebaseDatabase.getReference("trips");
+
         // Initialize RecyclerView
         recyclerView = rootView.findViewById(R.id.recycler_id);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         tripArrayList = new ArrayList <>();
-        row_index = 0;
-//        while (row_index <= 200) {
-//            Trip aTrip = new Trip( 1, "abc", "def", 30);
-//            tripArrayList.add(aTrip) ;
-//            row_index++;
-//        }
 
         // Initialize Adapter
         adapter = new ArrayTripAdapter(R.layout.trip_list_item, tripArrayList, getContext());
         recyclerView.setAdapter(adapter);
+
+        databaseReferenceToTrips.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                snapshot.getChildren().forEach(e->{
+                    tripArrayList.add(e.getValue(Trip.class));
+                });
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
 
         // Populate your dataset and update the adapter as needed
 
--- app/src/main/java/com/bidridego/viewholder/TripViewHolder.java
+++ app/src/main/java/com/bidridego/viewholder/TripViewHolder.java
@@ -2,6 +2,7 @@ package com.bidridego.viewholder;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,19 +10,22 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bidridego.R;
 
 public class TripViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-    public TextView cost;
-    public TextView destination;
-    public TextView source;
-    public TextView distance;
-    public TextView postedBy;
+    public TextView cost, destination, source, distance, postedBy, date, time, passengers;
+    public Switch isCarPool;
+//    public TextView distance;
 
     public TripViewHolder(View tripView) {
         super(tripView);
+        date = tripView.findViewById(R.id.date);
+        time = tripView.findViewById(R.id.time);
         cost = tripView.findViewById(R.id.cost);
-        destination = tripView.findViewById(R.id.source);
+        destination = tripView.findViewById(R.id.destination);
         source = tripView.findViewById(R.id.source);
 //        distance = tripView.findViewById(R.id.distance);
+        distance = tripView.findViewById(R.id.cost);
         postedBy = tripView.findViewById(R.id.username);
+        passengers = tripView.findViewById(R.id.seats);
+        isCarPool = tripView.findViewById(R.id.is_car_pool);
         tripView.setOnClickListener(this);
     }
 
